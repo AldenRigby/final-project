5cd51b562d901dbf0ad19eab54643174bc3b7027
AldenRigby <alden.rigby@ucas-edu.net>
2024-11-26 11:28:53 -0700

    more stuff ....

--- main.py
+++ main.py
@@ -5,28 +5,51 @@ import pygame
 pygame.mixer.init()
 sound1 = pygame.mixer.Sound('button.mp3')  # Load a sound.
 
+LEVEL_HITS = [2, 3] #how many seconds after program runs to get a hit.
+leniency = .2 #if hit is within this many seconds of a valid input then allow it
+secondsPerBeat = .5
+
 start = time.time()
+print("hit on the 5th and 7th beat")
 
 def getRuntime():
     return time.time() - start
 
 def background():
+    x = 0
     while True:
-        for x in range(10):
-            sys.stdout.write('\r'+str(x))
-            time.sleep(.2)
-            sys.stdout.flush()
+        x += 1
+
+        sys.stdout.write("\033[K")
+        print(x, end='\r')
+
+        sound1.play()
+        time.sleep(secondsPerBeat)
+
+        sys.stdout.flush()
 
 def handling_input(inp):
-    print('Got {}'.format(inp))
+    #print('Got {}'.format(inp), end="\r")
+    #print(getRuntime(), end="\r")
     sound1.play()
+
+    foundHit = False
+    for i in LEVEL_HITS:
+        if i + leniency > getRuntime() and i - leniency < getRuntime():
+            sys.stdout.write("\033[F")
+            print("    good job you hit", end="\r")
+            foundHit = True
+            break
+
+    if not foundHit:
+        sys.stdout.write("\033[F")
+        print("    bruh you didn't hit", end="\r")
 
 t = threading.Thread(target=background)
 t.daemon = True
 t.start()
 
 while True:
-    print(getRuntime())
     inp = input()
     handling_input(inp)
     if inp == 'q':
