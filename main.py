27c3a465533a31b4e287cb1dc9cb924957445a6d
AldenRigby <alden.rigby@ucas-edu.net>
2024-11-26 11:44:14 -0700

    wafe

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import time
 import threading
 import sys
 import pygame
+
 pygame.mixer.init()
 sound1 = pygame.mixer.Sound('button.mp3')  # Load a sound.
+#backgroundMusic = pygame.mixer.Sound('XXX.mp3')  # Load a sound.
+#hitEffect = pygame.mixer.Sound('XXX.mp3')  # Load a sound.
+#missEffect = pygame.mixer.Sound('XXX.mp3')  # Load a sound.
 
 LEVEL_HITS = [2, 3] #how many seconds after program runs to get a hit.
 leniency = .2 #if hit is within this many seconds of a valid input then allow it
 secondsPerBeat = .5
 
 start = time.time()
-print("hit on the 5th and 7th beat")
+print("enter on the 5th and 7th beat")
+print("\n\n\n\n")
+sys.stdout.write("\033[5F")
 
 def getRuntime():
     return time.time() - start
@@ -21,11 +27,16 @@ def background():
         x += 1
 
         sys.stdout.write("\033[K")
-        print(x, end='\r')
+        #print(x, end='\r')
+        print(f"""
+{x*"  |  "}{x}
+              
+              """)
 
         sound1.play()
         time.sleep(secondsPerBeat)
 
+        sys.stdout.write("\033[4F")
         sys.stdout.flush()
 
 def handling_input(inp):
@@ -53,5 +64,6 @@ while True:
     inp = input()
     handling_input(inp)
     if inp == 'q':
+        sys.stdout.write("\033[K")
         print('quitting')
         sys.exit()
