b0a46b1582159b2b51359d8585cd91cf5dd859f8
AldenRigby <alden.rigby@ucas-edu.net>
2024-12-04 22:50:44 -0700

    add sound effects and music and part of level

--- main.py
+++ main.py
@@ -19,20 +19,23 @@ import sys
 import pygame
 
 pygame.mixer.init() # setup pygame mixer
-sound1 = pygame.mixer.Sound('sounds/kick.mp3')  # Load a sound.
-#backgroundMusic = pygame.mixer.Sound('XXX.mp3')  # Load a sound.
-#hitEffect = pygame.mixer.Sound('XXX.mp3')  # Load a sound.
-#missEffect = pygame.mixer.Sound('XXX.mp3')  # Load a sound.
+beatEffect = pygame.mixer.Sound('sounds/kick.mp3')  # every beat this plays
+backgroundMusic = pygame.mixer.Sound('sounds/Samurai Techno.mp3')  # music
+blipEffect = pygame.mixer.Sound('sounds/blip.mp3')  # when player makes input not anywhere near the actual hit
+hitEffect = pygame.mixer.Sound('sounds/hit.mp3')  # plays when player is supposed to hit
+missEffect = pygame.mixer.Sound('sounds/miss.mp3')  # miss
+#drumEffect = pygame.mixer.Sound('sounds/drum.mp3')  # if player hits right then this should play soon after
 
 #how many seconds after program runs to start a 7 thing. //
 #how many seconds between beats for the 7 hit above thingy
-LEVEL_HITS        = [0,  4,   6] 
-LEVEL_HITS_TIMING = [.5, .25, .5]
+#samurai techno is 200 bpm. .6 for fast, .3 for medium, .15 for fast
+LEVEL_HITS        = [0,  4.8,9.6,14.4,16.8]
+LEVEL_HITS_TIMING = [.6, .6, .6, .15, .15]
 LEVEL_ACTUAL_HITS = [] # timing of when they actually have to hit
 for i in range(len(LEVEL_HITS)):
     LEVEL_ACTUAL_HITS.append(LEVEL_HITS[i] + LEVEL_HITS_TIMING[i]*6)
 
-leniency = .2 #if hit is within this many seconds (+-) of a valid input then allow it
+leniency = .1 #if hit is within this many seconds (+-) of a valid input then allow it
 secondsPerBeat = .5 #how many seconds are in each "beat" of the song.
 goodHits = 0 #how many times player got a good hit
 badHits = 0 #how many times player missed a note
@@ -40,6 +43,7 @@ levelIndex = 0 #what hit the program is on
 playerIndex = 0 #what hit the player is on (this should always be lower than or equal to levelindex)
 
 start = time.time() # set up the time at the start
+backgroundMusic.play()
 
 #set up graphics
 print("\nenter on the 7th beat\n")
@@ -57,7 +61,10 @@ def startHit(index): # this function starts a 1234567 thing
               """)
         sys.stdout.write("\033[3F")
         sys.stdout.flush()
-        sound1.play()
+        if i < 6:
+            beatEffect.play()
+        else:
+            hitEffect.play()
         time.sleep(LEVEL_HITS_TIMING[index])
 
 def updateScore():
@@ -85,6 +92,7 @@ def background(): # this function is always running in the backgroud. this lets 
                 #sys.stdout.write("\033[F")
                 sys.stdout.write("\033[K")
                 print("    missed ....", end="\r")
+                missEffect.play()
                 playerIndex += 1
                 badHits += 1
                 updateScore()
@@ -109,13 +117,15 @@ def handling_input(inp): # on player input
                 foundHit = True
                 break
             #if too far away from the hit, count as a miss
-        if i + leniency * 2 > getRuntime() and i - leniency * 2 < getRuntime():
+        if i + leniency * 3 > getRuntime() and i - leniency * 3 < getRuntime():
             sys.stdout.write("\033[F")
             sys.stdout.write("\033[K")
             #checks if the player already missed this note by pressing early
             if playerIndex > levelIndex:
+                blipEffect.play()
                 print("    already got that note", end="\r")
             else:
+                missEffect.play()
                 print("    that was close", end="\r")
                 playerIndex += 1
                 badHits = badHits + 1
@@ -124,6 +134,7 @@ def handling_input(inp): # on player input
     #if no hit then bleh
     if not foundHit:
         sys.stdout.write("\033[F")
+        blipEffect.play()
         print("    not even close buddy     ", end="\r")
         #badHits = badHits + 1
 
@@ -141,4 +152,5 @@ while True:
     if inp == 'q':
         sys.stdout.write("\033[K")
         print('quitting')
+        backgroundMusic.stop()
         sys.exit()
