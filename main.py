0c8f6e2c69cdcc6b8d9c7e65eb306be2e9d301c8
AldenRigby <alden.rigby@ucas-edu.net>
2024-12-09 11:49:28 -0700

    waef

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ import sys
 import pygame
 
 pygame.mixer.init() # setup pygame mixer
-beatEffect = pygame.mixer.Sound('sounds/kick.mp3')  # every beat this plays
-backgroundMusic = pygame.mixer.Sound('sounds/Samurai Techno.mp3')  # music
-blipEffect = pygame.mixer.Sound('sounds/blip.mp3')  # when player makes input not anywhere near the actual hit
-hitEffect = pygame.mixer.Sound('sounds/hit.mp3')  # plays when player is supposed to hit
-missEffect = pygame.mixer.Sound('sounds/miss.mp3')  # miss
-#drumEffect = pygame.mixer.Sound('sounds/drum.mp3')  # if player hits right then this should play soon after
+beatEffect = pygame.mixer.Sound('./sounds/kick.mp3')  # every beat this plays
+backgroundMusic = pygame.mixer.Sound('./sounds/Samurai Techno.mp3')  # music
+blipEffect = pygame.mixer.Sound('./sounds/blip.mp3')  # when player makes input not anywhere near the actual hit
+hitEffect = pygame.mixer.Sound('./sounds/hit.mp3')  # plays when player is supposed to hit
+missEffect = pygame.mixer.Sound('./sounds/miss.mp3')  # miss
+#drumEffect = pygame.mixer.Sound('./sounds/drum.mp3')  # if player hits right then this should play soon after
 
 
 #how many seconds after program runs to start a 7 thing. //
@@ -35,16 +35,16 @@ LEVEL_SECONDS_PER_BEAT = 60/LEVEL_BPM
 #LEVEL_HITS_BEAT        = [0, 32, 48, 51.16, 59.16]
 #LEVEL_HITS_TIMING_BEAT = [4, 2,  1, .5,    .5]
 #                                                                                the drop V                                                                                                                                   #slowdown V
-LEVEL_HITS        = [0,    9.6, 14.4, 16.55, 17.75, 19.25, 21.65, 24.05, 26.15, 27.35, 28.8, 30,  31.2, 32.4, 33.6, 34.8, 36,  37.2, 38.4, 39.6, 40.8, 42,  43.2, 44.4, 45.6, 46.8, 48,  49.2, 50.4, 51.6, 52.8, 54,  55.2, 56.4, 57.6, 58.95, 61,  62.35,  63.4]
-LEVEL_HITS_TIMING = [1.2, .6,  .3,   .15,   .15,   .3,   .3,   .3,      .15,   .15,   .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,   .15, .15,    .15]
+LEVEL_HITS        = [0,    9.6, 14.4, 16.55, 17.75, 19.25, 21.65, 24.05, 26.15, 27.35, 28.8, 30,  31.2, 32.4, 33.6, 34.8, 36,  37.2, 38.4, 39.6, 40.8, 42,  43.2, 44.4, 45.6, 46.8, 48,  49.2, 50.4, 51.6, 52.8, 54,  55.2, 56.4, 57.6, 58.95, 60,  61.35,  62.4,  63.75]
+LEVEL_HITS_TIMING = [1.2, .6,  .3,   .15,   .15,   .3,   .3,   .3,      .15,   .15,   .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,  .15, .15,  .15,  .15,  .15,   .15, .15,    .15,   .15]
 
 LEVEL_ACTUAL_HITS = [] # timing of when they actually have to hit
 for i in range(len(LEVEL_HITS)):
     LEVEL_ACTUAL_HITS.append(LEVEL_HITS[i] + LEVEL_HITS_TIMING[i]*6)
 
-leniency = .15 #if hit is within this many seconds (+-) of a valid input then allow it
-hitOffset = 0.07 #offset for hit after beat
-beatOffset = 0.11 #offset for beat in relation to music
+leniency = .1 #if hit is within this many seconds (+-) of a valid input then allow it
+hitOffset = 0 #offset for hit after beat
+beatOffset = 0 #offset for beat in relation to music
 #secondsPerBeat = .5 #how many seconds are in each "beat" of the song. replaced with levelhitstiming
 goodHits = 0 #how many times player got a good hit
 badHits = 0 #how many times player missed a note
@@ -83,6 +83,8 @@ def startHit(index): # this function starts a 1234567 thing
         if i < 7:
             time.sleep(LEVEL_HITS_TIMING[index]-hitOffset)
 
+    #move cursor using i
+
 def updateScore():
     #print score
     print("\n\n\n\n")
@@ -104,10 +106,10 @@ def background(): # this function is always running in the backgroud. this lets 
                 levelIndex = levelIndex + 1
 
         if playerIndex < len(LEVEL_ACTUAL_HITS) and playerIndex <= levelIndex:
-            if LEVEL_ACTUAL_HITS[playerIndex] < getRuntime() + leniency * 2:
+            if LEVEL_ACTUAL_HITS[playerIndex] < getRuntime() + leniency * 4:
                 #sys.stdout.write("\033[F")
                 sys.stdout.write("\033[K")
-                print("    missed ....", end="\r")
+                print("    missed ...." + str(playerIndex) + str(levelIndex), end="\r")
                 missEffect.play()
                 playerIndex += 1
                 badHits += 1
@@ -121,47 +123,49 @@ def handling_input(): # on player input
     global goodHits, badHits, playerIndex, levelIndex
     #check every allowed hit in the level
     foundHit = False
-    for i in LEVEL_ACTUAL_HITS:
-        if playerIndex <= levelIndex:
-            #if within a certain threshold, be nice and let them hit
-            if i + leniency > getRuntime() and i - leniency < getRuntime():
-                sys.stdout.write("\033[F")
-                sys.stdout.write("\033[K")
-                print(f"{getOffset(i)} good job you hit", end="\r")
-                playerIndex += 1
-                goodHits = goodHits + 1
-                foundHit = True
-                break
-            #if too far away from the hit, count as a miss
-        if i + leniency * 3 > getRuntime() and i - leniency * 3 < getRuntime():
+
+#SOMETHING HERE TODO HELP AHHHHHHH REPLACE ALL i WITH TIHS VAR DOESNT WORK
+    currentLevelHit = LEVEL_ACTUAL_HITS[playerIndex]
+
+    if playerIndex <= levelIndex:
+        #if within a certain threshold, be nice and let them hit
+        if currentLevelHit + leniency > getRuntime() and currentLevelHit - leniency < getRuntime():
             sys.stdout.write("\033[F")
             sys.stdout.write("\033[K")
-            #checks if the player already missed this note by pressing early
-            if playerIndex > levelIndex:
-                blipEffect.play()
-                print("    already got that note", end="\r")
-            else:
-                missEffect.play()
-                print(f"{getOffset(i)} that was close", end="\r")
-                playerIndex += 1
-                badHits = badHits + 1
+            print(f"{getOffset(currentLevelHit)} good job you hit", end="\r")
+            playerIndex += 1
+            goodHits += 1
             foundHit = True
-            break
+        #if too far away from the hit, count as a miss
+    if currentLevelHit + leniency * 3 > getRuntime() and currentLevelHit - leniency * 3 < getRuntime():
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
+        #checks if the player already missed this note by pressing early
+        if playerIndex <= levelIndex:
+            missEffect.play()
+            print(f"{getOffset(currentLevelHit)} that was close" + str(playerIndex) + str(levelIndex), end="\r")
+            playerIndex += 1
+            badHits += 1
+        else:
+            blipEffect.play()
+            print("    already got that note" + str(playerIndex) + str(levelIndex), end="\r")
+        foundHit = True
     #if no hit then bleh
     if not foundHit:
         sys.stdout.write("\033[F")
         blipEffect.play()
         print("    not even close buddy     ", end="\r")
         #badHits = badHits + 1
+    updateScore()
 
 #setup the background (idk how this works but stackoverflow does)
 t = threading.Thread(target=background)
 t.daemon = True
 t.start()
 
+updateScore()
 #check on userinputs
 while True:
-    updateScore()
     #input stuff
     inp = input()
     handling_input()
